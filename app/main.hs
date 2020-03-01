46456da945f8481080ebf03a886f3427be3fed6b
ibzaman <ibizapeanut@gmail.com>
2020-03-01 09:12:33 -0800

    add command line parsing of files

--- app/main.hs
+++ app/main.hs
@@ -1,6 +1,54 @@
+{-# LANGUAGE LambdaCase #-}
 module Main
-  ( main
-  ) where
+    ( main
+    )
+where
+
+import qualified Config.SystemdService         as SystemdService
+import qualified Data.Text                     as T
+import qualified Options.Applicative           as Args
+import qualified Parser                        as P
+import qualified System.IO                     as SIO
+
+
+data Arguments = ArgParseSystemdService T.Text
+
 
 main :: IO ()
-main = undefined
+main = arguments >>= \case
+    ArgParseSystemdService file ->
+        SIO.withFile (T.unpack file) SIO.ReadMode $ \handle ->
+            (   P.parse SystemdService.parse
+                <$> (T.pack <$> SIO.hGetContents handle)
+                )
+                >>= \case
+                        Left err ->
+                            putStrLn
+                                $  "Error while parsing file: "
+                                <> T.unpack err
+                        Right parsed -> print parsed
+
+
+arguments :: IO Arguments
+arguments = Args.execParser $ Args.info
+    (         Args.hsubparser
+            (Args.command
+                "parse"
+                (Args.info
+                    (Args.hsubparser
+                        (Args.command
+                            "systemdservice"
+                            (Args.info
+                                (   ArgParseSystemdService
+                                <$> Args.strArgument (Args.metavar "FILE")
+                                )
+                                mempty
+                            )
+                        )
+                    )
+                    (Args.fullDesc <> Args.progDesc "Parse a file")
+                )
+            )
+    Args.<**> Args.helper
+    )
+    (Args.fullDesc <> Args.progDesc "uconfig")
